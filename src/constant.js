5aee742a920e87b12f684d749404579419999ec1
xiaoyuhen <xiaoyuhen1994@gmail.com>
2019-09-03 17:11:10 +0800

    feat: add swiper

---
+++ src/constant.js
@@ -0,0 +1,262 @@
+const LIST = [
+  {
+    name: '文类件',
+    title: 'documents',
+    lists: [
+      {
+        name: '护照',
+        checked: false,
+      },
+      {
+        name: '护照复印件',
+        checked: false,
+      },
+      {
+        name: '台湾通行证',
+        checked: false,
+      },
+      {
+        name: '台湾通行证复印件',
+        checked: false,
+      },
+      {
+        name: '台湾签注',
+        checked: false,
+      },
+      {
+        name: '入台证彩色复印件',
+        checked: false,
+      },
+      {
+        name: '入台证',
+        checked: false,
+      },
+      {
+        name: '机票',
+        checked: false,
+      },
+      {
+        name: '台铁票',
+        checked: false,
+      },
+      {
+        name: '高铁票',
+        checked: false,
+      },
+      {
+        name: '保险单扫描件',
+        checked: false,
+      },
+      {
+        name: '酒店确认单',
+        checked: false,
+      },
+      {
+        name: '紧急联系人信息',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '衣物',
+    title: 'clothes',
+    lists: [
+      {
+        name: '换洗衣物',
+        checked: false,
+      },
+      {
+        name: '内衣',
+        checked: false,
+      },
+      {
+        name: '雨衣',
+        checked: false,
+      },
+      {
+        name: '睡衣',
+        checked: false,
+      },
+      {
+        name: '泳装',
+        checked: false,
+      },
+      {
+        name: '拖鞋',
+        checked: false,
+      },
+      {
+        name: '墨镜',
+        checked: false,
+      },
+      {
+        name: '防晒服',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '包',
+    title: 'bags',
+    lists: [
+      {
+        name: '背包',
+        checked: false,
+      },
+      {
+        name: '行李箱',
+        checked: false,
+      },
+      {
+        name: '钱包',
+        checked: false,
+      },
+       {
+        name: '相机包',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '日用品',
+    title: 'toiletries',
+    lists: [
+      {
+        name: '牙刷牙膏',
+        checked: false,
+      },
+      {
+        name: '牙线',
+        checked: false,
+      },
+      {
+        name: '洗发水、沐浴露',
+        checked: false,
+      },
+      {
+        name: '防晒霜',
+        checked: false,
+      },
+      {
+        name: '毛巾',
+        checked: false,
+      },
+      {
+        name: '纸巾',
+        checked: false,
+      },
+      {
+        name: '卫生巾',
+        checked: false,
+      },
+      {
+        name: '护肤品',
+        checked: false,
+      },
+      {
+        name: '化妆品',
+        checked: false,
+      },
+      {
+        name: '驱蚊剂',
+        checked: false,
+      },
+      {
+        name: '隐形眼镜',
+        checked: false,
+      },
+      {
+        name: '剃须用品',
+        checked: false,
+      },
+      {
+        name: '耳塞',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '旅行资金',
+    title: 'funds',
+    lists: [
+      {
+        name: '信用卡',
+        checked: false,
+      },
+      {
+        name: '台币',
+        checked: false,
+      },
+      {
+        name: '港币',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '电子用品',
+    title: 'funds',
+    lists: [
+      {
+        name: '手机和充电器',
+        checked: false,
+      },
+      {
+        name: '电脑和充电器',
+        checked: false,
+      },
+      {
+        name: '相机和充电器',
+        checked: false,
+      },
+      {
+        name: '台湾手机卡/出境 wifi',
+        checked: false,
+      },
+      {
+        name: '香港手机卡/出境 wifi',
+        checked: false,
+      },
+      {
+        name: '移动电源',
+        checked: false,
+      },
+      {
+        name: '耳机',
+        checked: false,
+      },
+      {
+        name: '相机',
+        checked: false,
+      },
+    ]
+  },
+  {
+    name: '杂物',
+    title: 'others',
+    lists: [
+      {
+        name: '晕车药',
+        checked: false,
+      },
+      {
+        name: '防水袋',
+        checked: false,
+      },
+      {
+        name: '保温杯',
+        checked: false,
+      },
+      {
+        name: '雨伞',
+        checked: false,
+      },
+      {
+        name: '眼罩',
+        checked: false,
+      },
+    ],
+  }
+]
+
+export default LIST
